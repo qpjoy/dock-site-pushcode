c261ae3716e892197863582cc8a37f990a1ad6e2
qpjoy996 <qpjoy996@163.com>
2024-08-12 08:29:43 +0800

    (fix): downloads url

--- src/features/Components/Download/index.tsx
+++ src/features/Components/Download/index.tsx
@@ -5,7 +5,7 @@ function Download({ style }: any) {
   const _download = (e: any) => {
     e.preventDefault();
     download(
-      "https://qp-diary-1320869019.cos.ap-shanghai.myqcloud.com/push-code/PushCode%E6%8E%A8%E6%B5%81%E5%8A%A9%E6%89%8B%20Setup%202.10.2.exe"
+      "https://memoscard.com/downloads/PushCode%E6%8E%A8%E6%B5%81%E5%8A%A9%E6%89%8B%20Setup%202.10.2.exe"
     );
   };
 
--- src/features/Components/Header/MenuList.tsx
+++ src/features/Components/Header/MenuList.tsx
@@ -12,7 +12,7 @@ function MenuList({ hyper_link, link_name, setMenuActive }: any) {
       // navigate("//#download");
       e.preventDefault();
       download(
-        "https://qp-diary-1320869019.cos.ap-shanghai.myqcloud.com/push-code/PushCode%E6%8E%A8%E6%B5%81%E5%8A%A9%E6%89%8B%20Setup%202.10.2.exe"
+        "https://memoscard.com/downloads/PushCode%E6%8E%A8%E6%B5%81%E5%8A%A9%E6%89%8B%20Setup%202.10.2.exe"
       );
     } else if (link_name === "使用教程") {
       e.preventDefault();
